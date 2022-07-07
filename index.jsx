1bf82209eb1f4e987ae69ab7e0ce9147fbb73d56
codymaverick81 <pawelfurman@gmail.com>
2022-07-07 19:33:52 +0200

    main

---
+++ index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+   <p>Chuj wam wszystkim głęboko w dupe</p>,
+   document.getElementById("root")
+);
---
+++ script.js
@@ -0,0 +1,15 @@
+//komentarz zbędny//
+var penis = "chuj Ci w dupę"
+//policzmy średnią//
+var num1 = 12;
+var num2 = 18;
+var num3 = 19;
+
+var srednia = srednia(num1,num2,num3);
+
+function srednia(num1,num2,num3) {
+    return (num1+num2+num3) / 3;
+}
+//drukarka robi brr//
+console.log("fiut" + 3 * 5);
+console.log(srednia);
